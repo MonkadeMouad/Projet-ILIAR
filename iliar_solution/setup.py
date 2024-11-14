aa0097b87a38b72c766f5d6706f5fb28f70c081a
Hamza Amrani <hamza.amrani8@etu.univ-lorraine.fr>
2024-11-14 17:19:07 +0100

    initiamisation paquets

---
+++ iliar_solution/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'iliar_solution'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.xml')),
+        ('share/' + package_name + '/config', glob('config/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Amrani',
+    maintainer_email='hamza.amrani8@etu.univ-lorraine.fr',
+    description='Description de votre paquet',
+    license='Licence de votre choix',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+)
+
