87348b65515af8ca3ecd19dfa5dd8581d623b6b3
Hamza Amrani <hamza.amrani8@etu.univ-lorraine.fr>
2024-12-19 13:43:26 +0100

    launch file reconfigured, need to do remapping

--- iliar_solution2/src/iliar_solution2/autopilot.py
+++ iliar_solution2/src/iliar_solution2/autopilot.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Float64
 import numpy as np
 import cv2
 import onnxruntime
+import os
 
 class Autopilot(Node):
     def __init__(self):
@@ -26,17 +27,17 @@ class Autopilot(Node):
 
         # Load the ONNX model
         try:
-            self.model = onnxruntime.InferenceSession("/home/profil/monkade1u/hammou/iliar_solution2/src/iliar_solution2/dummy_model.onnx")
+            self.model = onnxruntime.InferenceSession(os.path.expanduser("~/hammou/iliar_solution2/src/iliar_solution2/dummy_model.onnx"))
             self.get_logger().info("ONNX model loaded successfully.")
         except Exception as e:
             self.get_logger().error(f"Failed to load ONNX model: {e}")
 
-        self.sub_left = Subscriber(self, CompressedImage, "/left", qos_profile=qos_profile)
-        self.sub_front = Subscriber(self, CompressedImage, "/forward", qos_profile=qos_profile)
-        self.sub_right = Subscriber(self, CompressedImage, "/right", qos_profile=qos_profile)
+        self.sub_left = Subscriber(self, CompressedImage, "/audibot/left_camera/image_raw/compressed", qos_profile=qos_profile)
+        self.sub_front = Subscriber(self, CompressedImage, "/audibot/front_camera/image_raw/compressed", qos_profile=qos_profile)
+        self.sub_right = Subscriber(self, CompressedImage, "/audibot/right_camera/image_raw/compressed", qos_profile=qos_profile)
 
-        self.steering_pub = self.create_publisher(Float64, "/steer", 10)
-        self.throttle_pub = self.create_publisher(Float64, "/throttle", 10)
+        self.steering_pub = self.create_publisher(Float64, "/audibot/steering_cmd", 10)
+        self.throttle_pub = self.create_publisher(Float64, "/audibot/throttle_cmd", 10)
 
         self.sync = ApproximateTimeSynchronizer(
             [self.sub_left, self.sub_front, self.sub_right], queue_size=10, slop=0.1
@@ -52,9 +53,9 @@ class Autopilot(Node):
 
     def image_cb(self, left_msg, front_msg, right_msg):
         try:
-            left_img = self.bridge.compressed_imgmsg_to_cv2(left_msg, "bgr8")
-            front_img = self.bridge.compressed_imgmsg_to_cv2(front_msg, "bgr8")
-            right_img = self.bridge.compressed_imgmsg_to_cv2(right_msg, "bgr8")
+            left_img = self.bridge.compressed_imgmsg_to_cv2(left_msg, "rgb8")
+            front_img = self.bridge.compressed_imgmsg_to_cv2(front_msg, "rgb8")
+            right_img = self.bridge.compressed_imgmsg_to_cv2(right_msg, "rgb8")
 
             left_img = self.preprocess_frames(left_img)
             front_img = self.preprocess_frames(front_img)
@@ -70,7 +71,7 @@ class Autopilot(Node):
             self.steering_pub.publish(steering_msg)
 
             throttle_msg = Float64()
-            throttle_msg.data = 0.2
+            throttle_msg.data = 0.8
             self.throttle_pub.publish(throttle_msg)
         except Exception as e:
             self.get_logger().error(f"Error in image_cb: {e}")
