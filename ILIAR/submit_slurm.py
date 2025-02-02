a87e9de3ae7d4857c32ac70260d901746258671e
Hamza Amrani <amrnihamza@gmail.com>
2025-02-02 18:23:06 +0100

    long training

--- ILIAR/submit_slurm.py
+++ ILIAR/submit_slurm.py
@@ -9,8 +9,8 @@ def makejob(commit_id, configpath, nruns):
 
 #SBATCH --job-name=templatecode
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_prod_long
-#SBATCH --time=6:00:00
+#SBATCH --partition=gpu_prod_night
+#SBATCH --time=8:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
 #SBATCH --array=1-{nruns}
