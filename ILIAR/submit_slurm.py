366f0de93337460fb01a18a9307eee88d1d704e8
Hamza Amrani <amrnihamza@gmail.com>
2025-02-02 18:39:09 +0100

    12 hours shift

--- ILIAR/submit_slurm.py
+++ ILIAR/submit_slurm.py
@@ -10,7 +10,7 @@ def makejob(commit_id, configpath, nruns):
 #SBATCH --job-name=templatecode
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_prod_night
-#SBATCH --time= 12:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
 #SBATCH --array=1-{nruns}
